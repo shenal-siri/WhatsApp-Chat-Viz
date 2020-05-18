f097837ef93d6f43bd3f983f76573848a807ba77
Shenal Siripala <shenal.siri@gmail.com>
2020-05-17 22:33:21 -0400

    Minor refactoring and directory restructuring

--- Script-Generate-Chat-Data-Fake.py
+++ Script-Generate-Chat-Data-Fake.py
@@ -14,7 +14,7 @@ num_emojis = 30
 date_format = "[%Y-%m-%d, %I:%M:%S %p]"
 date_start = "[2018-12-21, 2:55:51 AM]"
 date_end = "[2020-04-04, 10:57:52 PM]"
-
+resource_path = os.path.join("resources", "lorem-ipsum.txt")
 
 # Create list of users (2 users currently supported)
 list_users = ["User 1", "User 2"]
@@ -22,7 +22,7 @@ list_users = ["User 1", "User 2"]
 
 # Extract complete list of words from lorem ipsum text file
 list_lorem_raw = []
-with open("lorem-ipsum.txt", "r") as f:
+with open(resource_path, "r") as f:
     for line in f:
         list_lorem_raw.extend(line.split())
 
@@ -30,18 +30,19 @@ with open("lorem-ipsum.txt", "r") as f:
 list_lorem = list(set(list_lorem_raw))
 
 # Get list of punctuation characters (excluding square bracket chars for less risk of datetime parse errors)
-list_punc = [punc for punc in string.punctuation if punc != ("[" or "]")]
+list_punc = [punc for punc in string.punctuation if (punc != "[" and punc != "]")]
 
-# Create list of predefined unicode emojis
+# Create list of randomly-selected unicode emojis
 list_emojis = random.sample(list(EMOJI_UNICODE.values()), num_emojis)
 
-# Create list of slang words
+# Create basic list of slang words
 list_slang = ['lool', 'looool', 'lmao', 'lmaoooo', 'lmaooo' 'wow', 'woooow', 'woowww', 'ha', 'hah', 'haha']
 
 # Create list for image / video / link messages
-list_msgtypes = ['\n\u200Eimage omitted\n', '\n\u200Evideo omitted\n', '\nhttps://www.google.com/\n', '\nhttps://github.com/shenal-siri\n']
+list_msgtypes = ['\n\u200Eimage omitted\n', '\n\u200Evideo omitted\n', '\nhttps://www.google.com/\n',
+                 '\nhttps://github.com/shenal-siri\n']
 
-# Create list of basic stopwords
+# Create basic list of stopwords
 list_stopwords = ['a', 'am', 'are', 'but', 'for', 'I', 'it' ,'is', 'on', 'that', 'the', 'you', 'was', 'were']
 
 # Create master list from which messages will be generated
@@ -64,7 +65,8 @@ timeseries = [dt.strftime(date_format) for dt in timeseries]
 # Create Faker object to generate a random sentence using words from the master list
 fake = Faker()
 
-# Output to the chat file, line-by-line, in the WhatsApp chat export format for iOS
+# Create the chat.txt file in the root folder
+# Chat is generated line-by-line, matching the WhatsApp chat export format (for iOS)
 with open("chat.txt", "w", encoding="utf-8") as f:
     for i in range(num_messages):
 
